11a154ab0bd05eb8c466d31848a1ba8c4407a499
unknown <saipranav36@gmail.com>
2019-10-01 10:24:10 -0500

    created four move methods

--- exercise-2.py
+++ exercise-2.py
@@ -5,3 +5,16 @@ class Aircraft:
         self.x=x
         self.y=y
         self.ac=ac
+    # created methods
+    def move_left(self):
+        print('moved left . . .')
+        self.x = self.x - self.ac
+    def move_right(self):
+        print('moved right . . .')
+        self.x = self.x + self.ac
+    def move_up(self):
+        print('moved up . . .')
+        self.y = self.y + self.ac
+    def move_down(self):
+        print('moved down')
+        self.y = self.y - self.ac
